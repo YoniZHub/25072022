789e394e4f9fec95a349ec7df9810eecef6d27f5
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-07-25 21:29:26 +0300

    Create SwitchCase.java

---
+++ SwitchCase.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter number");
+        int number = s.nextInt();
+
+//        if (number == 1) {
+//            System.out.println("One");
+//        } else if (number == 2) {
+//            System.out.println("Two");
+//        } else if (number == 3) {
+//            System.out.println("Three");
+//        }else {
+//            System.out.println("Unknown");
+//        }
+
+//        switch (number) {
+//            case 1:
+//                System.out.println("one");
+//                break;
+//            case 2:
+//                System.out.println("Two");
+//                break;
+//            case 3:
+//                System.out.println("Three");
+//                break;
+//        }
+
+
+
+
+
+    }
+}
