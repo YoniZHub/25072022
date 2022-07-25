4ed1c3ac62aa97a2ba44747143cd3829d9a46b3f
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-07-25 21:29:53 +0300

    Create SwapNumbers.java

---
+++ SwapNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner s = new Scanner(System.in);
+        //Swap values
+        int x = 5;
+        int y = 7;
+        int ezer = y;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        y = x;
+        x = ezer;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+
+    }
+}
